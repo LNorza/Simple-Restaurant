dea7406b1537d1d0b0a325806614e1daef3f6f1d
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-02-20 22:21:00 -0700

    Changes in the header design

--- web/src/utilities/arrayCards.js
+++ web/src/utilities/arrayCards.js
@@ -3,24 +3,24 @@ import inventory from "../assets/Images/inventario-dashboard.svg";
 import multipleUsers from "../assets/Images/MultipleUsers-Dashboard.svg";
 import analitycs from "../assets/Images/Analitycs-Dashboard.svg";
 export const arrayMenu = [
-    {
-        imagePath: command,
-        text: "Gestión de comandas",
-        direction: "/commands",
-    },
-    {
-        imagePath: inventory,
-        text: "Gestión de inventario",
-        direction: "/inventory",
-    },
-    {
-        imagePath: multipleUsers,
-        text: "Gestión de personal",
-        direction: "/personal",
-    },
-    {
-        imagePath: analitycs,
-        text: "Estadísticas",
-        direction: "/analitycs",
-    },
+	{
+		imagePath: command,
+		text: "Gestión de comandas",
+		direction: "/commands",
+	},
+	{
+		imagePath: inventory,
+		text: "Gestión de inventario",
+		direction: "/inventory",
+	},
+	{
+		imagePath: multipleUsers,
+		text: "Gestión de personal",
+		direction: "/personal",
+	},
+	{
+		imagePath: analitycs,
+		text: "Estadísticas",
+		direction: "/analitycs",
+	},
 ];

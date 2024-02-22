d20842298cf34ab41eaf13fc68ff9458781c59c7
LNorza <luiscnorza@gmail.com>
2024-02-22 11:34:14 -0600

    add header logo link to home

--- web/src/components/header/header.jsx
+++ web/src/components/header/header.jsx
@@ -4,23 +4,24 @@ import user from "../../assets/Images/user.svg";
 import "./header.css";
 
 export function Header() {
-	return (
-		<div className="header">
-			<div className="logo">
-				<Link to="/home">
-					<img src={logo} alt="simpleRestaurant" />
-				</Link>
-				<h1>SimpleRestaurant</h1>
-			</div>
-			<div className="user">
-				<img src={user} alt="user-foto" />
-				<div className="user-info">
-					<p>Samuel</p>
-					<p className="puesto">Gerente general</p>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="header">
+            <Link to="/home">
+                <div className="logo">
+                    <img src={logo} alt="simpleRestaurant" />
+
+                    <h1>SimpleRestaurant</h1>
+                </div>
+            </Link>
+            <div className="user">
+                <img src={user} alt="user-foto" />
+                <div className="user-info">
+                    <p>Samuel</p>
+                    <p className="puesto">Gerente general</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;

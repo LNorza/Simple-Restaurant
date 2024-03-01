96651132a72c6bc06898ae779459e5754f761f8d
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-03-01 16:26:59 -0700

    Added table in personal page

--- web/src/pages/command-page/commands.jsx
+++ web/src/pages/command-page/commands.jsx
@@ -1,14 +1,27 @@
+import {useEffect, useState} from "react";
 import Header from "../../components/header/header.jsx";
 import Navbar from "../../components/navbar/navbar.jsx";
 import "./commands.css";
 
 export function CommandsPage() {
-    return (
-        <div className="commands-page">
-            <Header />
-            <div>
-                <Navbar />
-            </div>
-        </div>
-    );
+	const [state, setState] = useState([]);
+	async function fetchData() {
+		const result = await fetch(
+			"https://simplerestaurant-api-production.up.railway.app/api/employees"
+		).then((response) => response.json());
+		setState(result);
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+	console.log(state);
+	return (
+		<div className="commands-page">
+			<Header />
+			<div>
+				<Navbar />
+			</div>
+		</div>
+	);
 }
--- web/src/pages/personal/personal.jsx
+++ web/src/pages/personal/personal.jsx
@@ -1,14 +1,69 @@
+import {useEffect, useState} from "react";
 import Header from "../../components/header/header.jsx";
 import Navbar from "../../components/navbar/navbar.jsx";
+import edit from "../../assets/Images/edit.svg";
+import trash from "../../assets/Images/trash.svg";
 import "./personal.css";
 
 export function PersonalPage() {
-    return (
-        <div className="personal-page">
-            <Header />
-            <div>
-                <Navbar />
-            </div>
-        </div>
-    );
+	const [state, setState] = useState([]);
+	async function fetchData() {
+		const result = await fetch(
+			"https://simplerestaurant-api-production.up.railway.app/api/employees"
+		).then((response) => response.json());
+		setState(result);
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+	console.log(state);
+
+	return (
+		<div className="personal-page">
+			<Header />
+			<div className="menu-container">
+				<Navbar />
+				<div className="principal-container">
+					<div className="header-container">
+						<h2>Gestión de personal</h2>
+					</div>
+					<div className="contenedor-tabla">
+						<div className="tabla">
+							<div className="tabla-encabezado">
+								<div className="texto-encabezado">Nombre</div>
+								<div className="texto-encabezado">NSS</div>
+								<div className="texto-encabezado">RFC</div>
+								<div className="texto-encabezado">Telefono</div>
+							</div>
+							<div className="contenedor-datos">
+								{state.map((employee) => (
+									<div
+										className="datos"
+										key={employee.IDEmpleado}
+									>
+										<span>
+											{employee.NombreEmp}{" "}
+											{employee.ApellidosEmp}
+										</span>
+										<span>{employee.NSS}</span>
+										<span>{employee.RFC}</span>
+										<span>{employee.Telefono}</span>
+										<div className="boton-container">
+											<button className="boton">
+												<img src={edit} alt="" />
+											</button>
+											<button className="boton">
+												<img src={trash} alt="" />
+											</button>
+										</div>
+									</div>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }

deb840d43ef2f7847b0993562eeb92f915f68199
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-03-10 15:47:49 -0700

    Changed style on EmployeeModal

--- web/src/components/employee-modal/EmployeeModal.jsx
+++ web/src/components/employee-modal/EmployeeModal.jsx
@@ -2,121 +2,122 @@ import {useEffect, useState} from "react";
 import "./EmployeeModal.css";
 
 export function EmployeeModal({isOpen, onClose, employeeID}) {
-    if (!isOpen) return null;
-    const [state, setState] = useState([]);
+	if (!isOpen) return null;
+	const [state, setState] = useState([]);
 
-    async function fetchData() {
-        const result = await fetch(
-            "https://simplerestaurant-api-production.up.railway.app/api/employees/" +
-                `${employeeID}`
-        ).then((response) => response.json());
-        setState(result);
-    }
+	async function fetchData() {
+		const result = await fetch(
+			"https://simplerestaurant-api-production.up.railway.app/api/employees/" +
+				`${employeeID}`
+		).then((response) => response.json());
+		setState(result);
+	}
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-    const onCloseModal = () => {
-        onClose();
-    };
+	const onCloseModal = () => {
+		onClose();
+	};
 
-    return (
-        <div className="modal">
-            <div onClick={onCloseModal} className="overlay"></div>
-            <div className="modal-container">
-                <div className="modal-header">
-                    Editar empleado
-                    <button onClick={onCloseModal}>✖</button>
-                </div>
+	return (
+		<div className="modal">
+			<div onClick={onCloseModal} className="overlay"></div>
+			<div className="modal-container">
+				<div className="modal-header">
+					Editar empleado
+					<button onClick={onCloseModal}>✖</button>
+				</div>
 
-                <div className="modal-form-content">
-                    <div className="container-inputs-emp">
-                        <div className="employee-ID">
-                            <label for="id">Empleado</label>
-                            <input
-                                type="text"
-                                value={employeeID}
-                                className="form-input emp"
-                            />
-                        </div>
-                        <div>
-                            <label for="Stand">Puesto</label>
-                            <input
-                                type="text"
-                                value={state.IDPuesto}
-                                className="form-input"
-                            />
-                        </div>
-                    </div>
+				<div className="modal-form-content">
+					<div className="container-inputs-emp">
+						<div className="employee-ID">
+							<label for="id">Empleado</label>
+							<input
+								type="text"
+								value={employeeID}
+								className="form-input"
+								disabled
+							/>
+						</div>
+						<div>
+							<label for="Stand">Puesto</label>
+							<input
+								type="text"
+								value={state.IDPuesto}
+								className="form-input"
+							/>
+						</div>
+					</div>
 
-                    <div className="container-inputs-emp">
-                        <div>
-                            <label for="name">Nombre</label>
-                            <input
-                                type="text"
-                                value={state.NombreEmp}
-                                className="form-input"
-                            />
-                        </div>
+					<div className="container-inputs-emp">
+						<div>
+							<label for="name">Nombre</label>
+							<input
+								type="text"
+								value={state.NombreEmp}
+								className="form-input"
+							/>
+						</div>
 
-                        <div>
-                            <label for="lastName">Apellidos</label>
-                            <input
-                                type="text"
-                                value={state.ApellidosEmp}
-                                className="form-input"
-                            />
-                        </div>
-                    </div>
+						<div>
+							<label for="lastName">Apellidos</label>
+							<input
+								type="text"
+								value={state.ApellidosEmp}
+								className="form-input"
+							/>
+						</div>
+					</div>
 
-                    <div className="container-inputs-emp">
-                        <div>
-                            <label for="nss">NSS</label>
-                            <input
-                                type="text"
-                                value={state.NSS}
-                                className="form-input"
-                            />
-                        </div>
-                        <div>
-                            <label for="rfc">RFC</label>
-                            <input
-                                type="text"
-                                value={state.RFC}
-                                className="form-input"
-                            />
-                        </div>
-                    </div>
-                    <label for="phone">Teléfono</label>
-                    <input
-                        type="text"
-                        value={state.Telefono}
-                        className="form-input"
-                    />
-                    <label for="address">Dirección</label>
-                    <input
-                        type="text"
-                        value={state.Direccion}
-                        className="form-input"
-                    />
-                    <label for="email">Email</label>
-                    <input
-                        type="text"
-                        value={state.Email}
-                        className="form-input"
-                    />
-                </div>
-                <div className="form-button-container">
-                    <button
-                        type="submit"
-                        className="form-button"
-                        onClick={onClose}
-                    >
-                        Actualizar empleado
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
+					<div className="container-inputs-emp">
+						<div>
+							<label for="nss">NSS</label>
+							<input
+								type="text"
+								value={state.NSS}
+								className="form-input"
+							/>
+						</div>
+						<div>
+							<label for="rfc">RFC</label>
+							<input
+								type="text"
+								value={state.RFC}
+								className="form-input"
+							/>
+						</div>
+					</div>
+					<label for="phone">Teléfono</label>
+					<input
+						type="text"
+						value={state.Telefono}
+						className="form-input"
+					/>
+					<label for="address">Dirección</label>
+					<input
+						type="text"
+						value={state.Direccion}
+						className="form-input"
+					/>
+					<label for="email">Email</label>
+					<input
+						type="text"
+						value={state.Email}
+						className="form-input"
+					/>
+				</div>
+				<div className="form-button-container">
+					<button
+						type="submit"
+						className="form-button"
+						onClick={onClose}
+					>
+						Actualizar empleado
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }

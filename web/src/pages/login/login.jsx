315b8dd9dc0b6fd11e4f7edbbb5a120443aea43b
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-03-24 12:49:46 -0700

    Changed icons

--- web/src/pages/login/login.jsx
+++ web/src/pages/login/login.jsx
@@ -4,43 +4,43 @@ import logo from "../../assets/Images/Logo.svg";
 import "./login.css";
 
 export function Login() {
-    const handlesubmit = (e) => {
-        e.preventDefault();
+	const handlesubmit = (e) => {
+		e.preventDefault();
 
-        //Llamada a la API para hacer el login
-        //Validacion de datos
-        //Redirección a la pagina de inicio
-    };
+		//Llamada a la API para hacer el login
+		//Validacion de datos
+		//Redirección a la pagina de inicio
+	};
 
-    return (
-        <div className="login-container">
-            <section className="login-main-container">
-                <div className="img-login">
-                    <img src={loginImage} alt="login" />
-                </div>
+	return (
+		<div className="login-container">
+			<section className="login-main-container">
+				<div className="img-login">
+					<img src={loginImage} alt="login" />
+				</div>
 
-                <article className="form-login">
-                    <img src={logo} alt="Logo" />
-                    <h1>Acceso</h1>
+				<article className="form-login">
+					<img src={logo} alt="Logo" />
+					<h1>Acceso</h1>
 
-                    <form onSubmit={handlesubmit}>
-                        <label htmlFor="">Nombre del usuario</label>
-                        <input
-                            type="text"
-                            placeholder="Introduce tú nombre de usuario"
-                        />
+					<form onSubmit={handlesubmit}>
+						<label htmlFor="">Nombre del usuario</label>
+						<input
+							type="text"
+							placeholder="Introduce tú nombre de usuario"
+						/>
 
-                        <label htmlFor="">Contraseña</label>
-                        <input
-                            type="password"
-                            placeholder="Introduce tú contraseña"
-                        />
-                        <Link to="/home">
-                            <button>Entrar</button>
-                        </Link>
-                    </form>
-                </article>
-            </section>
-        </div>
-    );
+						<label htmlFor="">Contraseña</label>
+						<input
+							type="password"
+							placeholder="Introduce tú contraseña"
+						/>
+						<Link to="/home" className="link">
+							<button>Entrar</button>
+						</Link>
+					</form>
+				</article>
+			</section>
+		</div>
+	);
 }

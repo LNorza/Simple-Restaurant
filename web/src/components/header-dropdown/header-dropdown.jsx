30e2dcde8af9aba87a051be128b352dac48d2e99
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-03-26 16:40:52 -0700

    Fixed bug on dropdown link

--- web/src/components/header-dropdown/header-dropdown.jsx
+++ web/src/components/header-dropdown/header-dropdown.jsx
@@ -5,49 +5,52 @@ import "./header-dropdown.css";
 import {useState} from "react";
 
 export function HeaderDropdown() {
-    const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-    const openDropdown = () => {
-        if (isOpen) {
-            setIsOpen(false);
-            return;
-        }
+	const openDropdown = () => {
+		if (isOpen) {
+			setIsOpen(false);
+			return;
+		}
 
-        setIsOpen(true);
-    };
+		setIsOpen(true);
+	};
 
-    const closeDropdown = () => {
-        setIsOpen(false);
-    };
+	const closeDropdown = () => {
+		setIsOpen(false);
+	};
 
-    return (
-        <>
-            {isOpen && (
-                <div onClick={closeDropdown} className="exit-Dropdown" />
-            )}
+	return (
+		<>
+			{isOpen && (
+				<div onClick={closeDropdown} className="exit-Dropdown" />
+			)}
 
-            <div onClick={openDropdown} className="dropdown">
-                <button className="dropbtn">
-                    <img src={user} alt="user-image" />
-                    <div className="user-info">
-                        <p>Samuel</p>
-                        <p className="stand">Gerente general</p>
-                    </div>
-                </button>
+			<div onClick={openDropdown} className="dropdown">
+				<button className="dropbtn">
+					<img src={user} alt="user-image" />
+					<div className="user-info">
+						<p>Samuel</p>
+						<p className="stand">Gerente general</p>
+					</div>
+				</button>
 
-                <div className={`dropdown-menu ${isOpen ? "open" : ""}`}>
-                    <button className="dropdown-menu-button">
-                        <User />
-                        <span>Perfil</span>
-                    </button>
-                    <Link to="/">
-                        <button className="dropdown-menu-button">
-                            <LogOut />
-                            <span>Salir</span>
-                        </button>
-                    </Link>
-                </div>
-            </div>
-        </>
-    );
+				<div className={`dropdown-menu ${isOpen ? "open" : ""}`}>
+					<button className="dropdown-menu-button">
+						<Link className="dropdown-link">
+							<User />
+							<span>Perfil</span>
+						</Link>
+					</button>
+
+					<button className="dropdown-menu-button">
+						<Link to="/" className="dropdown-link">
+							<LogOut />
+							<span>Salir</span>
+						</Link>
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }

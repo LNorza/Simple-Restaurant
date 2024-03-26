b2ffae94fa7dfbd4215b33ad8c96f2b4fb39da4d
LNorza <luiscnorza@gmail.com>
2024-03-26 14:40:10 -0600

    Add toggle on user

--- web/src/components/header-dropdown/header-dropdown.jsx
+++ web/src/components/header-dropdown/header-dropdown.jsx
@@ -2,30 +2,36 @@ import user from "../../assets/Images/user.svg";
 import {User, LogOut} from "lucide-react";
 import {Link} from "react-router-dom";
 import "./header-dropdown.css";
+import {useState} from "react";
 
 export function HeaderDropdown() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className="dropdown">
-            <button className="dropbtn">
+            <button className="dropbtn" onClick={toggleDropdown}>
                 <img src={user} alt="user-image" />
                 <div className="user-info">
                     <p>Samuel</p>
                     <p className="stand">Gerente general</p>
                 </div>
             </button>
-            <div className="dropdown-menu">
-                <div>
+
+            <div className={`dropdown-menu ${isOpen ? "open" : ""}`}>
+                <button className="dropdown-menu-button">
+                    <User />
+                    <span>Perfil</span>
+                </button>
+                <Link to="/">
                     <button className="dropdown-menu-button">
-                        <User />
-                        <span>Perfil</span>
+                        <LogOut />
+                        <span>Salir</span>
                     </button>
-                    <Link to="/">
-                        <button className="dropdown-menu-button">
-                            <LogOut />
-                            <span>Salir</span>
-                        </button>
-                    </Link>
-                </div>
+                </Link>
             </div>
         </div>
     );

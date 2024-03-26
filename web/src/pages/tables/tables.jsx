46f92fca6b925d018ddf2cd367ad14fcf078440f
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-03-25 19:52:17 -0700

    Added headers, add button and cards to the tables page

--- web/src/pages/tables/tables.jsx
+++ web/src/pages/tables/tables.jsx
@@ -1,13 +1,35 @@
 import Header from "../../components/header/header.jsx";
 import Navbar from "../../components/navbar/navbar.jsx";
+import {CirclePlus} from "lucide-react";
+import {MenuCard} from "../../components/menu-card/menu-card.jsx";
+import {arrayTables} from "../../utilities/arrayTables.js";
 import "./tables.css";
 
 export function TablesPage() {
 	return (
-		<div className="tables-page">
+		<div>
 			<Header />
 			<div className="menu-container">
 				<Navbar />
+				<div className="principal-container">
+					<div className="header-container">
+						<h2>Gestión de mesas</h2>
+						<button className="add-button">
+							<CirclePlus size={20} />
+							Agregar Mesa
+						</button>
+					</div>
+					<div className="card-container">
+						{arrayTables.map(({imagePath, text, direction}) => (
+							<MenuCard
+								key={text}
+								imagePath={imagePath}
+								text={text}
+								direction={direction}
+							/>
+						))}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
---
+++ web/src/utilities/arrayTables.js
@@ -0,0 +1,28 @@
+import table from "../assets/Images/table-dashboard.svg";
+export const arrayTables = [
+	{
+		imagePath: table,
+		text: "Mesa #1",
+		direction: "",
+	},
+	{
+		imagePath: table,
+		text: "Mesa #2",
+		direction: "",
+	},
+	{
+		imagePath: table,
+		text: "Mesa #3",
+		direction: "",
+	},
+	{
+		imagePath: table,
+		text: "Mesa #4",
+		direction: "",
+	},
+	{
+		imagePath: table,
+		text: "Mesa #5",
+		direction: "",
+	},
+];
